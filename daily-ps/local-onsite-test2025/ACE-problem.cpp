1980b556da541c136ca3e64c50f761cee1af9f1c
Rawan-Ahmed <rawanahmednour.287@gmail.com>
2025-07-03 21:39:15 +0300

    ACE

---
+++ daily-ps/local-onsite-test2025/ACE-problem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   long long n ;
+    cin>> n;
+    if(n%2==0){
+        cout<<"Mansour";
+    }else{
+    cout<<"Youssef"; }
+    return 0;
+}
